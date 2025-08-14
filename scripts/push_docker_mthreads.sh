8bbae929fabb0c353fae896a1a569dac136fcf68
Xiaodong Ye <xiaodong.ye@mthreads.com>
2025-08-14 11:23:38 +0800

    Address review comments
    Signed-off-by: Xiaodong Ye <xiaodong.ye@mthreads.com>

--- scripts/push_docker_mthreads.sh
+++ scripts/push_docker_mthreads.sh
@@ -6,18 +6,18 @@ set -eu
 
 for FLAVOR in $FLAVORS; do
     MANIFEST_TAG="${FINAL_IMAGE_REPO}:${VERSION}-${FLAVOR}"
+    ARCH=$(basename "${PLATFORM}")
     case "$FLAVOR" in
         musa)
             if [ "$PLATFORM" = "linux/amd64" ]; then
                 MUSAVERSION="rc4.2.0"
-                ARCH=$(basename "${PLATFORM}")
                 BASE_TAG="${MANIFEST_TAG}-${ARCH}"
                 VERSIONED_TAG="${MANIFEST_TAG}-${MUSAVERSION}-${ARCH}"
                 LATEST_TAG="${FINAL_IMAGE_REPO}:latest"
 
+                echo "Pushing $VERSIONED_TAG and $LATEST_TAG"
                 docker tag "$BASE_TAG" "$VERSIONED_TAG"
                 docker push "$VERSIONED_TAG"
-
                 docker tag "$BASE_TAG" "$LATEST_TAG"
                 docker push "$LATEST_TAG"
             else
@@ -30,13 +30,14 @@ for FLAVOR in $FLAVORS; do
                 AMD64_TAG="${MANIFEST_TAG}-amd64"
                 ARM64_TAG="${MANIFEST_TAG}-arm64"
 
+                echo "Pushing $MANIFEST_TAG"
                 docker manifest rm "$MANIFEST_TAG" || true
                 docker manifest create "$MANIFEST_TAG" "$AMD64_TAG" "$ARM64_TAG"
                 docker manifest push "$MANIFEST_TAG"
             else
-                ARCH=$(basename "${PLATFORM}")
                 TAG="${MANIFEST_TAG}-${ARCH}"
 
+                echo "Pushing $TAG"
                 docker push "$TAG"
             fi
             ;;
