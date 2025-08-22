c40f3a70e1d8b7235b104bbc3523b83d06747e70
Xiaodong Ye <xiaodong.ye@mthreads.com>
2025-08-22 10:44:13 +0800

    refine
    Signed-off-by: Xiaodong Ye <xiaodong.ye@mthreads.com>

--- discover/gpu_info_vulkan.c
+++ discover/gpu_info_vulkan.c
@@ -55,9 +55,9 @@ void vk_init(char* vk_lib_path, vk_init_resp_t *resp) {
   appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
   appInfo.pNext = NULL;
   appInfo.pApplicationName = "Ollama";
-  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.applicationVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
   appInfo.pEngineName = "No Engine";
-  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.engineVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
   appInfo.apiVersion = VK_API_VERSION_1_2;
 
   VkInstanceCreateInfo createInfo = {};
--- discover/gpu_info_vulkan.h
+++ discover/gpu_info_vulkan.h
@@ -4,99 +4,62 @@
 
 #include "gpu_info.h"
 
-// DEPRECATED: This define is deprecated. VK_API_VERSION_MAJOR should be used instead.
-#define VK_VERSION_MAJOR(version) ((uint32_t)(version) >> 22)
-
-// DEPRECATED: This define is deprecated. VK_API_VERSION_MINOR should be used instead.
-#define VK_VERSION_MINOR(version) (((uint32_t)(version) >> 12) & 0x3FFU)
-
-// DEPRECATED: This define is deprecated. VK_API_VERSION_PATCH should be used instead.
-#define VK_VERSION_PATCH(version) ((uint32_t)(version) & 0xFFFU)
-// Compatibility macros for older Vulkan versions
-#ifndef VK_API_VERSION_MAJOR
-#define VK_API_VERSION_MAJOR VK_VERSION_MAJOR
-#define VK_API_VERSION_MINOR VK_VERSION_MINOR
-#define VK_API_VERSION_PATCH VK_VERSION_PATCH
-#endif
-
-// DEPRECATED: This define is deprecated. VK_MAKE_API_VERSION should be used instead.
-#define VK_MAKE_VERSION(major, minor, patch) \
-    ((((uint32_t)(major)) << 22) | (((uint32_t)(minor)) << 12) | ((uint32_t)(patch)))
-
+// Just enough typedef's to dlopen/dlsym for memory information
 #define VK_MAKE_API_VERSION(variant, major, minor, patch) \
     ((((uint32_t)(variant)) << 29) | (((uint32_t)(major)) << 22) | (((uint32_t)(minor)) << 12) | ((uint32_t)(patch)))
-
-// DEPRECATED: This define has been removed. Specific version defines (e.g. VK_API_VERSION_1_0), or the VK_MAKE_VERSION macro, should be used instead.
-//#define VK_API_VERSION VK_MAKE_VERSION(1, 0, 0) // Patch version should always be set to 0
-// Vulkan 1.2 version number
-#define VK_API_VERSION_1_2 VK_MAKE_API_VERSION(0, 1, 2, 0)// Patch version should always be set to 0
-#define VK_DEFINE_HANDLE(object) typedef struct object##_T* object;
+#define VK_API_VERSION_1_2                VK_MAKE_API_VERSION(0, 1, 2, 0)
+#define VK_API_VERSION_MAJOR(version)     (((uint32_t)(version) >> 22U) & 0x7FU)
+#define VK_API_VERSION_MINOR(version)     (((uint32_t)(version) >> 12U) & 0x3FFU)
+#define VK_API_VERSION_PATCH(version)     ((uint32_t)(version) & 0xFFFU)
+#define VK_DEFINE_HANDLE(object)          typedef struct object##_T* object;
 #define VK_MAX_PHYSICAL_DEVICE_NAME_SIZE  256U
 #define VK_UUID_SIZE                      16U
 #define VK_MAX_EXTENSION_NAME_SIZE        256U
 #define VK_MAX_MEMORY_TYPES               32U
 #define VK_MAX_MEMORY_HEAPS               16U
-#define VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME "VK_KHR_get_physical_device_properties2"
-
-/*
-***************************************************************************************************
-*   Platform-specific directives and type declarations
-***************************************************************************************************
-*/
-
-/* Platform-specific calling convention macros.
- *
- * Platforms should define these so that Vulkan clients call Vulkan commands
- * with the same calling conventions that the Vulkan implementation expects.
- *
- * VKAPI_ATTR - Placed before the return type in function declarations.
- *              Useful for C++11 and GCC/Clang-style function attribute syntax.
- * VKAPI_CALL - Placed after the return type in function declarations.
- *              Useful for MSVC-style calling convention syntax.
- * VKAPI_PTR  - Placed between the '(' and '*' in function pointer types.
- *
- * Function declaration:  VKAPI_ATTR void VKAPI_CALL vkCommand(void);
- * Function pointer type: typedef void (VKAPI_PTR *PFN_vkCommand)(void);
- */
-#if defined(_WIN32)
-    // On Windows, Vulkan commands use the stdcall convention
-    #define VKAPI_ATTR
-    #define VKAPI_CALL __stdcall
-    #define VKAPI_PTR  VKAPI_CALL
-#elif defined(__ANDROID__) && defined(__ARM_ARCH) && __ARM_ARCH < 7
-    #error "Vulkan is not supported for the 'armeabi' NDK ABI"
-#elif defined(__ANDROID__) && defined(__ARM_ARCH) && __ARM_ARCH >= 7 && defined(__ARM_32BIT_STATE)
-    // On Android 32-bit ARM targets, Vulkan functions use the "hardfloat"
-    // calling convention, i.e. float parameters are passed in registers. This
-    // is true even if the rest of the application passes floats on the stack,
-    // as it does by default when compiling for the armeabi-v7a NDK ABI.
-    #define VKAPI_ATTR __attribute__((pcs("aapcs-vfp")))
-    #define VKAPI_CALL
-    #define VKAPI_PTR  VKAPI_ATTR
-#else
-    // On other platforms, use the default calling convention
-    #define VKAPI_ATTR
-    #define VKAPI_CALL
-    #define VKAPI_PTR
-#endif
+#define VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME \
+    "VK_KHR_get_physical_device_properties2"
+#define VKAPI_PTR
 
 VK_DEFINE_HANDLE(VkInstance)
 VK_DEFINE_HANDLE(VkPhysicalDevice)
 
 typedef uint32_t VkBool32;
 typedef uint64_t VkDeviceSize;
-
-typedef enum VkMemoryHeapFlagBits {
-    VK_MEMORY_HEAP_DEVICE_LOCAL_BIT = 0x00000001,
-} VkMemoryHeapFlagBits;
-
+typedef uint32_t VkFlags;
+typedef VkFlags  VkSampleCountFlags;
+typedef VkFlags  VkMemoryHeapFlags;
+typedef VkFlags  VkMemoryPropertyFlags;
+typedef VkFlags  VkInstanceCreateFlags;
 
 typedef enum VkResult {
     VK_SUCCESS = 0,
 } VkResult;
 
-typedef uint32_t VkFlags;
-typedef VkFlags VkSampleCountFlags;
+typedef enum VkMemoryHeapFlagBits {
+    VK_MEMORY_HEAP_DEVICE_LOCAL_BIT = 0x00000001,
+} VkMemoryHeapFlagBits;
+
+typedef enum VkPhysicalDeviceType {
+    VK_PHYSICAL_DEVICE_TYPE_CPU = 4
+} VkPhysicalDeviceType;
+
+typedef enum VkStructureType {
+    VK_STRUCTURE_TYPE_APPLICATION_INFO = 0,
+    VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO = 1,
+    VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_MEMORY_PROPERTIES_2 = 1000059006,
+    VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_MEMORY_BUDGET_PROPERTIES_EXT = 1000237000,
+} VkStructureType;
+
+typedef enum VkSystemAllocationScope {
+    VK_SYSTEM_ALLOCATION_SCOPE_COMMAND = 0,
+} VkSystemAllocationScope;
+
+typedef enum VkInternalAllocationType {
+    VK_INTERNAL_ALLOCATION_TYPE_EXECUTABLE = 0,
+    VK_INTERNAL_ALLOCATION_TYPE_MAX_ENUM   = 0x7FFFFFFF
+} VkInternalAllocationType;
+
 typedef struct VkPhysicalDeviceLimits {
     uint32_t              maxImageDimension1D;
     uint32_t              maxImageDimension2D;
@@ -206,10 +169,6 @@ typedef struct VkPhysicalDeviceLimits {
     VkDeviceSize          nonCoherentAtomSize;
 } VkPhysicalDeviceLimits;
 
-typedef enum VkPhysicalDeviceType {
-    VK_PHYSICAL_DEVICE_TYPE_CPU = 4
-} VkPhysicalDeviceType;
-
 typedef struct VkPhysicalDeviceSparseProperties {
     VkBool32    residencyStandard2DBlockShape;
     VkBool32    residencyStandard2DMultisampleBlockShape;
@@ -217,6 +176,7 @@ typedef struct VkPhysicalDeviceSparseProperties {
     VkBool32    residencyAlignedMipSize;
     VkBool32    residencyNonResidentStrict;
 } VkPhysicalDeviceSparseProperties;
+
 typedef struct VkPhysicalDeviceProperties {
     uint32_t                            apiVersion;
     uint32_t                            driverVersion;
@@ -234,21 +194,12 @@ typedef struct VkExtensionProperties {
     uint32_t    specVersion;
 } VkExtensionProperties;
 
-typedef enum VkStructureType {
-    VK_STRUCTURE_TYPE_APPLICATION_INFO = 0,
-    VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO = 1,
-    VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_MEMORY_PROPERTIES_2 = 1000059006,
-    VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_MEMORY_BUDGET_PROPERTIES_EXT = 1000237000,
-} VkStructureType;
-
 typedef struct VkPhysicalDeviceMemoryBudgetPropertiesEXT {
     VkStructureType    sType;
     void*              pNext;
     VkDeviceSize       heapBudget[VK_MAX_MEMORY_HEAPS];
     VkDeviceSize       heapUsage[VK_MAX_MEMORY_HEAPS];
 } VkPhysicalDeviceMemoryBudgetPropertiesEXT;
-
-typedef VkFlags VkInstanceCreateFlags;
 
 typedef struct VkApplicationInfo {
     VkStructureType    sType;
@@ -259,6 +210,7 @@ typedef struct VkApplicationInfo {
     uint32_t           engineVersion;
     uint32_t           apiVersion;
 } VkApplicationInfo;
+
 typedef struct VkInstanceCreateInfo {
     VkStructureType             sType;
     const void*                 pNext;
@@ -270,23 +222,11 @@ typedef struct VkInstanceCreateInfo {
     const char* const*          ppEnabledExtensionNames;
 } VkInstanceCreateInfo;
 
-
-typedef enum VkSystemAllocationScope {
-    VK_SYSTEM_ALLOCATION_SCOPE_COMMAND = 0,
-    VK_SYSTEM_ALLOCATION_SCOPE_OBJECT = 1,
-    VK_SYSTEM_ALLOCATION_SCOPE_CACHE = 2,
-    VK_SYSTEM_ALLOCATION_SCOPE_DEVICE = 3,
-    VK_SYSTEM_ALLOCATION_SCOPE_INSTANCE = 4,
-    VK_SYSTEM_ALLOCATION_SCOPE_MAX_ENUM = 0x7FFFFFFF
-} VkSystemAllocationScope;
-
 typedef void* (VKAPI_PTR *PFN_vkAllocationFunction)(
     void*                                       pUserData,
     size_t                                      size,
     size_t                                      alignment,
     VkSystemAllocationScope                     allocationScope);
-
-
 
 typedef void* (VKAPI_PTR *PFN_vkReallocationFunction)(
     void*                                       pUserData,
@@ -295,17 +235,11 @@ typedef void* (VKAPI_PTR *PFN_vkReallocationFunction)(
     size_t                                      alignment,
     VkSystemAllocationScope                     allocationScope);
 
-typedef enum VkInternalAllocationType {
-    VK_INTERNAL_ALLOCATION_TYPE_EXECUTABLE = 0,
-    VK_INTERNAL_ALLOCATION_TYPE_MAX_ENUM = 0x7FFFFFFF
-} VkInternalAllocationType;
-
 typedef void (VKAPI_PTR *PFN_vkFreeFunction)(
     void*                                       pUserData,
     void*                                       pMemory);
 
-
-    typedef void (VKAPI_PTR *PFN_vkInternalFreeNotification)(
+typedef void (VKAPI_PTR *PFN_vkInternalFreeNotification)(
     void*                                       pUserData,
     size_t                                      size,
     VkInternalAllocationType                    allocationType,
@@ -316,7 +250,8 @@ typedef void (VKAPI_PTR *PFN_vkInternalAllocationNotification)(
     size_t                                      size,
     VkInternalAllocationType                    allocationType,
     VkSystemAllocationScope                     allocationScope);
-    typedef struct VkAllocationCallbacks {
+
+typedef struct VkAllocationCallbacks {
     void*                                   pUserData;
     PFN_vkAllocationFunction                pfnAllocation;
     PFN_vkReallocationFunction              pfnReallocation;
@@ -325,27 +260,22 @@ typedef void (VKAPI_PTR *PFN_vkInternalAllocationNotification)(
     PFN_vkInternalFreeNotification          pfnInternalFree;
 } VkAllocationCallbacks;
 
-typedef VkFlags VkMemoryPropertyFlags;
-
 typedef struct VkMemoryType {
     VkMemoryPropertyFlags    propertyFlags;
     uint32_t                 heapIndex;
 } VkMemoryType;
 
-typedef VkFlags VkMemoryHeapFlags;
-
 typedef struct VkMemoryHeap {
     VkDeviceSize         size;
     VkMemoryHeapFlags    flags;
 } VkMemoryHeap;
+
 typedef struct VkPhysicalDeviceMemoryProperties {
     uint32_t        memoryTypeCount;
     VkMemoryType    memoryTypes[VK_MAX_MEMORY_TYPES];
     uint32_t        memoryHeapCount;
     VkMemoryHeap    memoryHeaps[VK_MAX_MEMORY_HEAPS];
 } VkPhysicalDeviceMemoryProperties;
-
-
 
 typedef struct VkPhysicalDeviceMemoryProperties2 {
     VkStructureType                     sType;
@@ -354,41 +284,40 @@ typedef struct VkPhysicalDeviceMemoryProperties2 {
 } VkPhysicalDeviceMemoryProperties2;
 
 typedef struct {
-  void* vk_handle;
-  uint16_t verbose;
+    void* vk_handle;
+    uint16_t verbose;
 
-  VkInstance vk;
-  int num_devices;
+    VkInstance vk;
+    int num_devices;
 
-  void (*vkGetPhysicalDeviceProperties)(
-    VkPhysicalDevice                            physicalDevice,
-    VkPhysicalDeviceProperties*                 pProperties);
-  VkResult (*vkEnumerateDeviceExtensionProperties)(
-      VkPhysicalDevice                            physicalDevice,
-      const char*                                 pLayerName,
-      uint32_t*                                   pPropertyCount,
-      VkExtensionProperties*                      pProperties);
-  VkResult (*vkCreateInstance)(
-      const VkInstanceCreateInfo*                 pCreateInfo,
-      const VkAllocationCallbacks*                pAllocator,
-      VkInstance*                                 pInstance);
-  VkResult (*vkEnumeratePhysicalDevices)(
-      VkInstance                                  instance,
-      uint32_t*                                   pPhysicalDeviceCount,
-      VkPhysicalDevice*                           pPhysicalDevices);
-  void (*vkGetPhysicalDeviceMemoryProperties2)(
-      VkPhysicalDevice                            physicalDevice,
-      VkPhysicalDeviceMemoryProperties2*          pMemoryProperties);
-  void (*vkDestroyInstance)(
-      VkInstance                                  instance,
-      const VkAllocationCallbacks*                pAllocator);
+    void (*vkGetPhysicalDeviceProperties)(
+        VkPhysicalDevice                            physicalDevice,
+        VkPhysicalDeviceProperties*                 pProperties);
+    VkResult (*vkEnumerateDeviceExtensionProperties)(
+        VkPhysicalDevice                            physicalDevice,
+        const char*                                 pLayerName,
+        uint32_t*                                   pPropertyCount,
+        VkExtensionProperties*                      pProperties);
+    VkResult (*vkCreateInstance)(
+        const VkInstanceCreateInfo*                 pCreateInfo,
+        const VkAllocationCallbacks*                pAllocator,
+        VkInstance*                                 pInstance);
+    VkResult (*vkEnumeratePhysicalDevices)(
+        VkInstance                                  instance,
+        uint32_t*                                   pPhysicalDeviceCount,
+        VkPhysicalDevice*                           pPhysicalDevices);
+    void (*vkGetPhysicalDeviceMemoryProperties2)(
+        VkPhysicalDevice                            physicalDevice,
+        VkPhysicalDeviceMemoryProperties2*          pMemoryProperties);
+    void (*vkDestroyInstance)(
+        VkInstance                                  instance,
+        const VkAllocationCallbacks*                pAllocator);
 } vk_handle_t;
 
-typedef struct vk_init_resp
-{
-  char *err; // If err is non-null handle is invalid
-  int num_devices;
-  vk_handle_t ch;
+typedef struct vk_init_resp {
+    char *err; // If err is non-null handle is invalid
+    int num_devices;
+    vk_handle_t ch;
 } vk_init_resp_t;
 
 void vk_init(char* vk_lib_path, vk_init_resp_t *resp);
